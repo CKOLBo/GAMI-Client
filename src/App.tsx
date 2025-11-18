4ed3738e3b7137aff307577cf7ba9c10d56c8b4d
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-18 15:19:00 +0900

    refactor: SignupPage를 PasswordPage로 경로 변경
    - /signup 경로에 PasswordPage 컴포넌트 연결
    - @ 경로 별칭 사용으로 import 통일

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
-import SignupPage from './pages/signup/step1/SignupPage';
+import Password from '@/pages/signup/password/PasswordPage';
 import JoinPage from './pages/signup/join/JoinPage';
 import EmailPage from './pages/signup/email/EmailPage';
 
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signup" element={<Password />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/email" element={<EmailPage />} />
       </Routes>
