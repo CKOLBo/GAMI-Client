fd4e015ff767dce80476ebfd4f321ff4bc7438f1
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-28 14:53:52 +0900

    feat: App.tsx 라우팅 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '@/pages/login/LoginPage';
-import Password from '@/pages/signup/signup2/SignupPasswordPage';
-import JoinPage from '@/pages/signup/signup1/SignupJoinPage';
+import Step3 from '@/pages/signup/signup3/SignupStep3';
+import Step1 from '@/pages/signup/signup1/SignupStep1';
 
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup/join" element={<JoinPage />} />
-        <Route path="/signup/password" element={<Password />} />
+        <Route path="/signup1" element={<Step1 />} />
+        <Route path="/signup3" element={<Step3 />} />
       </Routes>
     </BrowserRouter>
   );
