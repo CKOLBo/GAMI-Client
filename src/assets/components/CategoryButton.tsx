1d6fbc29089c39778c7c7f9f8e94c479c957d6a9
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-30 02:46:18 +0900

    style: border 두께 변경

--- src/assets/components/CategoryButton.tsx
+++ src/assets/components/CategoryButton.tsx
@@ -14,8 +14,8 @@ export default function CategoryButton({
       onClick={onClick}
       className={`px-[12px] py-[12px] rounded-full text-[14px] font-[500] transition-all duration-200 ${
         isSelected
-          ? 'bg-[#BFA9FF] border-[#BFA9FF] text-[#ffffff]'
-          : 'bg-[#ffffff] text-[#6D6F79] border-2 border-[#B7BCC8] hover:bg-[#AA8EFF]'
+          ? 'bg-[#BFA9FF] border-[#BFA9FF] border-[2px] text-[#ffffff]'
+          : 'bg-[#ffffff] text-[#6D6F79] border-[2px] border-[#B7BCC8] hover:bg-[#AA8EFF]'
       }`}
     >
       {label}
