4638b1cffabe55041b5e96b386d00337bad5d87e
koreahghg <koreahghg@gmail.com>
2025-11-30 04:17:53 +0900

    feat: MainPage에 사이드바 컴포넌트 적용

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,7 +1,12 @@
+import Sidebar from '@/components/Sidebar';
+
 export default function MainPage() {
   return (
-    <div>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">
 
+      </div>
     </div>
   );
 }
