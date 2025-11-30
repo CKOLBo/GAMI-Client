4513b1ea17184255c9707e63082e577402c8774a
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-30 13:04:19 +0900

    style: text white로 style 변경

--- src/assets/components/GenderButton.tsx
+++ src/assets/components/GenderButton.tsx
@@ -17,7 +17,7 @@ export default function GenderButton({
       onClick={onClick}
       className={`border-[1px] border-[solid] rounded-[6px] text-[14px] font-[500] p-[16px] w-[49%] cursor-pointer transition-all duration-[300ms] outline-none ${
         isSelected
-          ? 'bg-[#BFA9FF] text-white border-[#BFA9FF]'
+          ? 'bg-[#BFA9FF] text-[white] border-[#BFA9FF]'
           : 'bg-[white] border-[#B7BCC8] text-[#6D6F79] hover:border-[#BFA9FF] hover:text-[#BFA9FF]'
       }`}
     >
