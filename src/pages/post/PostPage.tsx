797f6cc499df236098014dc799cbab07ad1dc996
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-08 08:08:48 +0900

    feat: post 상단 header 부분 퍼블리싱

---
+++ src/pages/post/PostPage.tsx
@@ -0,0 +1,17 @@
+import InputSearch from '@/assets/components/InputSearch';
+import Button from '@/assets/components/Button';
+export default function PostPage() {
+  return (
+    <div className="flex flex-row my-25">
+      <div>
+        <p className="text-[40px] w-46 h-12 font-bold mx-25">익명 게시판</p>
+      </div>
+      <div>
+        <InputSearch />
+      </div>
+      <div>
+        <Button text="글 쓰기" to="" />
+      </div>
+    </div>
+  );
+}
