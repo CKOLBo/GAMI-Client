c6308e9d99be6fa403feb2a0d2e89c548530586d
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-07 21:53:28 +0900

    feat: button text 경로 props로 받기

--- src/assets/components/Button.tsx
+++ src/assets/components/Button.tsx
@@ -1,11 +1,20 @@
-export default function Button() {
+import { useNavigate } from 'react-router-dom';
+
+interface ButtonProps {
+  text: string;
+  to: string;
+}
+
+export default function Button({ text, to }: ButtonProps) {
+  const navigate = useNavigate();
   return (
     <div>
       <button
+        onClick={() => navigate(to)}
         type="button"
         className="rounded-[10px] bg-main-1 w-38 h-16 text-white font-bold text-2xl"
       >
-        글 쓰기
+        {text}
       </button>
     </div>
   );
