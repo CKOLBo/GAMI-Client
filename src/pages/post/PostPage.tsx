b155f880987b09eef90f4399570c8441a0b9010a
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-09 00:33:59 +0900

    feat: Button 추가 및 Post 컴포넌트 적용

--- src/pages/post/PostPage.tsx
+++ src/pages/post/PostPage.tsx
@@ -1,17 +1,26 @@
 import InputSearch from '@/assets/components/InputSearch';
 import Button from '@/assets/components/Button';
+import Post from '@/assets/components/Post';
 export default function PostPage() {
   return (
-    <div className="flex flex-row my-25">
-      <div>
-        <p className="text-[40px] w-46 h-12 font-bold mx-25">익명 게시판</p>
+    <>
+      <div className="flex flex-row my-25">
+        <div>
+          <p className="text-[40px] w-46 h-12 font-bold mx-25">익명 게시판</p>
+        </div>
+        <div>
+          <InputSearch />
+        </div>
+        <div className="ml-114 flex">
+          <Button text="글 쓰기" to="" />
+        </div>
+        <div className="ml-14">
+          <Button text="내가 쓴 글" to="" />
+        </div>
       </div>
       <div>
-        <InputSearch />
+        <Post />
       </div>
-      <div className="ml-114">
-        <Button text="글 쓰기" to="" />
-      </div>
-    </div>
+    </>
   );
 }
