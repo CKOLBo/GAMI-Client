4fea00ffe74bdabf6e487f5611ccf2510f19cde7
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-08 23:58:05 +0900

    feat: svg 파일 추가 Hart svg 클릭시 색상 변경

--- src/assets/components/Post.tsx
+++ src/assets/components/Post.tsx
@@ -1,15 +1,35 @@
+import { useState } from 'react';
+import Hart from '@/assets/svg/Hart';
+import Comment from '@/assets/svg/Comment';
+import Report from '@/assets/svg/Report';
+
 export default function Post() {
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
-    <div>
+    <div className="flex flex-row justify-between">
       <div>
-        <p>제목이 들어갈 곳</p>
-        <p>내용이 들어갈 곳</p>
+        <div>
+          <p className="font-bold text-[32px]">제목이 들어갈 곳</p>
+          <p className="text-gray-3 text-2xl font-bold">내용이 들어갈 곳</p>
+        </div>
+        <div>
+          <p>익명</p>
+          <p>1시간 전</p>
+        </div>
       </div>
-      <div>
-        <p>익명</p>
-        <p>1시간 전</p>
+
+      <div className="flex flex-row">
+        <button className="h-[28px]" onClick={() => setIsLiked(!isLiked)}>
+          <Hart isSelect={isLiked} />
+        </button>
+        <button className="h-[28px]">
+          <Comment />
+        </button>
+        <button className="h-[28px]">
+          <Report />
+        </button>
       </div>
-      <div></div>
     </div>
   );
 }
