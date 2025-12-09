9af2c2e794d65c6d5f7f4d32011641fd91e8e1ea
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-09 18:08:02 +0900

    fix: 단위 vw 뷰포트 단위로 변경

--- src/pages/post/PostPage.tsx
+++ src/pages/post/PostPage.tsx
@@ -1,6 +1,7 @@
 import InputSearch from '@/assets/components/InputSearch';
 import Button from '@/assets/components/Button';
 import Post from '@/assets/components/Post';
+
 export default function PostPage() {
   const posts = [
     {
@@ -43,34 +44,39 @@ export default function PostPage() {
 
   return (
     <div className="w-full">
-      <div className="flex flex-col lg:flex-row items-start lg:items-center my-6 lg:my-22 px-4 lg:px-0 gap-6 lg:gap-0">
-        <div className="w-full lg:w-auto">
-          <p className="text-3xl lg:text-[40px] font-bold lg:mx-25">
-            익명 게시판
-          </p>
-        </div>
-        <div className="flex flex-col sm:flex-row w-full lg:w-auto items-stretch sm:items-center gap-4 lg:gap-114">
-          <div className="w-full sm:flex-1 lg:w-auto">
-            <InputSearch />
+      <div className="max-w-[75vw] mx-auto px-[0.83vw] lg:px-[1.25vw]">
+        <div className="flex flex-col lg:flex-row items-start lg:items-center my-[0.83vw] sm:my-[1.25vw] lg:my-[3.33vw] gap-[0.83vw] lg:gap-[5.21vw]">
+          <div className="w-full lg:w-auto shrink-0">
+            <p className="text-[1.25vw] sm:text-[1.56vw] lg:text-[2.08vw] xl:text-[2.08vw] font-bold">
+              익명 게시판
+            </p>
           </div>
-          <div className="flex gap-3 lg:gap-14">
-            <Button text="글 쓰기" to="/post/write" />
-            <Button text="내가 쓴 글" to="/post/my" />
+
+          <div className="flex flex-col sm:flex-row w-full flex-wrap items-stretch sm:items-center gap-[0.63vw] sm:gap-[0.83vw] justify-between">
+            <div className="w-full sm:flex-1 max-w-[20.83vw]">
+              <InputSearch />
+            </div>
+
+            <div className="flex flex-wrap gap-[0.63vw] sm:gap-[0.83vw] lg:gap-[1.25vw]">
+              <Button text="글 쓰기" to="/post/write" />
+              <Button text="내가 쓴 글" to="/post/my" />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="px-4 lg:px-0">
-        {posts.map((post) => (
-          <Post
-            key={post.id}
-            title={post.title}
-            content={post.content}
-            author={post.author}
-            timeAgo={post.timeAgo}
-            likeCount={post.likeCount}
-            commentCount={post.commentCount}
-          />
-        ))}
+
+        <div className="border-t-2 border-gray-2">
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              title={post.title}
+              content={post.content}
+              author={post.author}
+              timeAgo={post.timeAgo}
+              likeCount={post.likeCount}
+              commentCount={post.commentCount}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
