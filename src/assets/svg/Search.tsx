38f76324e323645c533682309add3e679bef6813
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-09 09:39:21 +0900

    refactor: Search svg CamelCase 적용

--- src/assets/svg/Search.tsx
+++ src/assets/svg/Search.tsx
@@ -10,9 +10,9 @@ export default function Search() {
       <path
         d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
         stroke="#6D6F79"
-        stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
