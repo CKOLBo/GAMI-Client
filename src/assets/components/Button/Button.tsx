35d4acb1c99cc6b6587c189cf0b811ff714e662d
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-11 17:50:57 +0900

    refactor: onClick 핸들러를 받을 수 있도록 수정하고, to prop은 선택적으로 만들어 다양한 상황에서 재사용할 수 있도록 개선

--- src/assets/components/Button/Button.tsx
+++ src/assets/components/Button/Button.tsx
@@ -2,20 +2,25 @@ import { useNavigate } from 'react-router-dom';
 
 interface ButtonProps {
   text: string;
-  to: string;
+  to?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ text, to }: ButtonProps) {
+export default function Button({ text, to, onClick }: ButtonProps) {
   const navigate = useNavigate();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (onClick) onClick(e);
+    if (to) navigate(to);
+  };
+
   return (
-    <div>
-      <button
-        onClick={() => navigate(to)}
-        type="button"
-        className="rounded-[10px] bg-main-1 w-38 h-16 cursor-pointer text-white font-bold text-2xl"
-      >
-        {text}
-      </button>
-    </div>
+    <button
+      onClick={handleClick}
+      type="button"
+      className="rounded-[10px] bg-main-1 w-38 h-16 cursor-pointer text-white font-bold text-2xl"
+    >
+      {text}
+    </button>
   );
 }
