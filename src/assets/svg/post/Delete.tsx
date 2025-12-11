b83365c4ed0f3550d9b59b46209c4ea155a4f25f
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-11 17:56:28 +0900

    refactor: Delete camel case 적용

--- src/assets/svg/post/Delete.tsx
+++ src/assets/svg/post/Delete.tsx
@@ -10,9 +10,9 @@ export default function Delete() {
       <path
         d="M19 13.9444V23.2778M12 13.9444V23.2778M5 7.72222V26.3889C5 27.214 5.36875 28.0053 6.02513 28.5888C6.6815 29.1722 7.57174 29.5 8.5 29.5H22.5C23.4283 29.5 24.3185 29.1722 24.9749 28.5888C25.6312 28.0053 26 27.214 26 26.3889V7.72222M1.5 7.72222H29.5M6.75 7.72222L10.25 1.5H20.75L24.25 7.72222"
         stroke="#333D48"
-        stroke-width="3"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
