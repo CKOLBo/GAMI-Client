c8cdf31687293c0273d8dd416261de7428999f36
koreahghg <koreahghg@gmail.com>
2025-12-15 09:47:35 +0900

    feat: 사이드바 적용

--- src/pages/mentoring/MentoringPage.tsx
+++ src/pages/mentoring/MentoringPage.tsx
@@ -3,7 +3,8 @@ import Sidebar from '@/assets/components/Sidebar';
 export default function MentoringPage() {
     return(
         <div className="flex">
-            <Sidebar />            
+            <Sidebar />
+                      
         </div>
     );
 }
