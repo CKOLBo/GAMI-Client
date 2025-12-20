3e79cd9f53d226ac860ae9d6fb55f59146868dc0
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-20 14:44:33 +0900

    style: tailwind 기본 클래스로 변경

--- src/pages/admin/AdminHead.tsx
+++ src/pages/admin/AdminHead.tsx
@@ -2,7 +2,7 @@ import Line from '@/assets/components/Line';
 
 export default function AdminHead() {
   return (
-    <div className="border flex rounded-lg w-full max-w-[1300px] h-[80px] px-12 py-5 border-gray-1">
+    <div className="border flex rounded-lg w-full max-w-[1300px] h-20 px-12 py-5 border-gray-1">
       <span className="text-2xl flex items-center pr-12 text-gray-1 font-semibold">
         번호
       </span>
