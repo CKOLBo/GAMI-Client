912125ffefbaa0c03ac429eeb7378dfbfc53f6f8
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-22 12:06:43 +0900

    style: tailwind 기본 클래스로 변경

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -46,10 +46,10 @@ export default function MyPage() {
       <div className="flex-1 flex flex-col justify-center items-center">
         <div className="flex flex-col items-center mb-8 w-full max-w-2xl">
           <div className="mb-6 2xl:mb-8">
-            <ProfileIcon className="w-[160px] h-[160px]" />
+            <ProfileIcon className="w-40 h-40" />
           </div>
 
-          <div className="flex items-center gap-[10px]">
+          <div className="flex items-center gap-2.5">
             <button
               type="button"
               onClick={() => setIsPasswordModalOpen(true)}
@@ -89,14 +89,14 @@ export default function MyPage() {
               <button
                 type="button"
                 onClick={handleCategoryCancel}
-                className="p-[10px] w-20 h-15 border cursor-pointer border-gray-2 text-xl text-main-3 rounded-xl hover:bg-gray-50 transition-colors bg-white"
+                className="p-2.5 w-20 h-15 border cursor-pointer border-gray-2 text-xl text-main-3 rounded-xl hover:bg-gray-50 transition-colors bg-white"
               >
                 취소
               </button>
               <button
                 type="button"
                 onClick={handleCategorySave}
-                className="p-[10px] w-20 h-15 bg-main-2 text-white text-xl rounded-xl hover:bg-main-2-hover transition-colors cursor-pointer"
+                className="p-2.5 w-20 h-15 bg-main-2 text-white text-xl rounded-xl hover:bg-main-2-hover transition-colors cursor-pointer"
               >
                 수정
               </button>
