95db97dcb18f29d5e857084ef297d84d72308f73
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-23 10:30:44 +0900

    style: InputSearch 컴포넌트의 테두리 색상 수정

--- src/assets/components/Input/InputSearch.tsx
+++ src/assets/components/Input/InputSearch.tsx
@@ -18,7 +18,7 @@ export default function InputSearch({
         placeholder={placeholder}
         value={value}
         onChange={onChange}
-        className="w-100 h-16 pl-16 pr-6 rounded-full border border-[#D6D6D7] bg-[#F9F9F9] text-2xl font-bold outline-none focus:border-[#BFA9FF]"
+        className="w-100 h-16 pl-16 pr-6 rounded-full border border-gray-4 bg-[#F9F9F9] text-2xl font-bold outline-none focus:border-main-1"
       />
       <div className="absolute left-8 px-5 top-1/2 -translate-1/2">
         <Search />
