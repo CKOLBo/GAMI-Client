2d57bf553d5960d9e975552701daff6ebdb43c13
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-23 13:55:29 +0900

    feat: 게시글 헤더 컴포넌트에 키워드 검색 기능 추가

--- src/assets/components/post/PostHead.tsx
+++ src/assets/components/post/PostHead.tsx
@@ -1,13 +1,28 @@
 import Button from '../Button/Button';
 import InputSearch from '../Input/InputSearch';
 
-export default function PostHead() {
+interface PostHeadProps {
+  keyword: string;
+  onKeywordChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearch: () => void;
+}
+
+export default function PostHead({
+  keyword,
+  onKeywordChange,
+  onSearch,
+}: PostHeadProps) {
   return (
     <div className="flex flex-col lg:flex-row items-start lg:items-center my-4 sm:my-6 lg:my-16 gap-3 lg:gap-20">
       <div className="flex flex-col sm:flex-row w-full flex-wrap sm:items-center gap-3 sm:gap-4 justify-between">
         <div className="w-full sm:flex-1 max-w-100">
-          <InputSearch />
+          <InputSearch
+            value={keyword}
+            onChange={onKeywordChange}
+            onEnter={onSearch}
+          />
         </div>
+
         <div className="flex pl-[410px]">
           <Button text="글 쓰기" to="/post-write" />
         </div>
